d29f7619d106479ef34d234e8ee7447ee4ab6f92
jdender <jdenderplays@gmail.com>
2017-12-19 19:57:13 -0500

    chore: Fix everything

--- src/main.js
+++ src/main.js
@@ -1,39 +1,43 @@
 const fs = require('fs');
-const ms = require('ms');
-let db;
 
-function setDB(path, db) {
-    fs.readFile(path, 'utf8', function(err, data) {
-        if(err) {
-            fs.writeFileSync(path, JSON.stringify({}, null, 2))
+module.exports = class JSONDatabase {
+    constructor(path) {
+        this.path = path;
+        this._store = {};
+    }
+
+    async sync() {
+        let database = await fs.readFileSync(this.path, 'utf8');
+
+        if (!database) {
+            await fs.writeFileSync(this.path, JSON.stringify({}));
+            database = {};
         } else {
-            return db = JSON.parse(data)
-        }
-        fs.writeFileSync(path, JSON.stringify(db, null, 2))
-    })
-}
 
-module.exports = function() {
-        return {
-            store: db,
-            start: function(path) {
-                setInterval(function() {
-                setDB(path, db)
-            }, ms('5s'))},
-            set: function(item, value) {
-                db[item] = value
-                if(db[item] === value) {
-                    return `Success set ${item}'s data to ${value}`;
-                } else {
-                    return `Error! ${item} was not successfully set to ${value}`;
-                };
-            },
-            get: function(item) {
-                if(db[item] === '') {
-                    return 'Item Empty'
-                } else {
-                return db[item]
-            }
+            database = JSON.parse(database);
+        }
+
+        this._store = Object.assign(database, this._store);
+        console.log(this._store)
+
+        await fs.writeFileSync(this.path, JSON.stringify(this._store))
+    }
+
+    async get(key) {
+        if (!!this._store[key] === false) {
+            return 'Item Empty'
+        } else {
+            return this._store[key]
+        }
+    }
+
+    async set(key, value) {
+        this._store[key] = value;
+
+        if (this._store[item] === value) {
+            return `Success set ${item}'s data to ${value}`;
+        } else {
+            return `Error! ${item} was not successfully set to ${value}`;
         }
     }
 }
